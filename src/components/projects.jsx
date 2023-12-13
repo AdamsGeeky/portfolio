4f58c1c6932f148b0eb4120bb463cdd7053bea45
ADAMU MUHAMMAD MUHAMMAD <muhdadamu0008@gmail.com>
2023-12-13 12:43:52 +0100

    feat: update

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -16,7 +16,7 @@ const ProjectLists = [
     description: 'SkillBoost is a platform where students can get full access to their mentorship and much more.',
     src: '/assets/02.png',
     tags: ['React','Infima', 'Firebase', 'NodeJS', 'Google maps' ],
-    href: 'https://gsunavis.web.app/',
+    href: 'https://gdscgsu.web.app/',
     codeHref: 'https://github.com/AdamsGeeky/skillbost',
   },
   {
