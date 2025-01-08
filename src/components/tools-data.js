ae52673d13b8350b7979422c2644fcc4ed3b090a
ADAMU MUHAMMAD MUHAMMAD <muhdadamu0008@gmail.com>
2025-01-08 10:53:41 +0100

    Update tools-data.js

--- src/components/tools-data.js
+++ src/components/tools-data.js
@@ -15,14 +15,6 @@ export const toolsData = [
         src: 'https://docusaurus.io/img/docusaurus-social-card.png',
       },
       {
-        name: 'Chakra UI',
-        src: 'https://pbs.twimg.com/profile_images/1244925541448286208/rzylUjaf_400x400.jpg',
-      },
-      {
-        name: 'Infima',
-        src: 'https://infima.dev/img/logo.png',
-      },
-      {
         name: 'TailwindCSS',
         src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2048px-Tailwind_CSS_Logo.svg.png',
       },
@@ -30,8 +22,6 @@ export const toolsData = [
         name: 'Bootstrap',
         src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png',
       }
-
-
     ],
   },
   {
@@ -42,14 +32,13 @@ export const toolsData = [
         src: 'https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_f0b606abb6d19089febc9faeeba5bc05/nodejs-development-services.png',
       },
       {
-        name: 'Python',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/800px-Python-logo-notext.svg.png',
-      },
-      {
         name: 'express',
         src: 'https://expressjs.com/images/express-facebook-share.png',
       },
-
+      {
+        name: 'FASTAPI',
+        src: 'https://img.shields.io/badge/FastAPI-005571?style=for-the-badge&logo=fastapi)&nbsp',
+      }
     ],
   },
   {
@@ -63,7 +52,6 @@ export const toolsData = [
         name: 'MongoDB',
         src: 'https://www.tutorialsteacher.com/Content/images/home/mongodb.svg',
       },
-
       {
         name: 'Prisma',
         src: 'https://plugins.jetbrains.com/files/14240/159812/icon/pluginIcon.png',
@@ -77,14 +65,47 @@ export const toolsData = [
         name: 'Git',
         src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/1200px-Git-logo.svg.png',
       },
+       {
+        name: 'Linux',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/a/af/Tux.png',
+      },
+      {
+        name: 'ViM',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/9/9f/Vimlogo.svg',
+      },
       {
         name: 'GitHub',
         src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png',
       },
       {
         name: 'Docker',
-        src:'https://upload.wikimedia.org/wikipedia/commons/7/79/Docker_%28container_engine%29_logo.png',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/7/79/Docker_%28container_engine%29_logo.png',
       }
+    ],
+  },
+  {
+    title: 'Programming Languages',
+    tools: [
+      {
+        name: 'C',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/1/18/C_Programming_Language.svg',
+      },
+      {
+        name: 'Python',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg',
+      },
+      {
+        name: 'Rust',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Rust_programming_language_black_logo.svg',
+      },
+      {
+        name: 'JavaScript',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+      },
+      {
+        name: 'TypeScript',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg',
+      },
     ],
   },
 ];
